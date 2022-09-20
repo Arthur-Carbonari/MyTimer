f16bc1f8e67198ff70e07beb272067be17483263
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-20 15:38:49 -0600

    Add Scanner object to take input without having to use args for testing

--- MyTimer/src/Main.java
+++ MyTimer/src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		String testInput = in.nextLine();
 		
+		String[] mockArgs = {};
+		
+		if(!testInput.isEmpty()) mockArgs = new String[] {testInput};
+						
 	}
 
 }
