dae20665287816435e32bbae00f91d97e60f4231
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 11:14:28 -0600

    Create ArgsParserTest Unit test

---
+++ MyTimer/src/ArgsParserTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArgsParserTest {
+
+	@Test
+	void testIsArgsValid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testParseArgsToIntArray() {
+		fail("Not yet implemented");
+	}
+
+}
