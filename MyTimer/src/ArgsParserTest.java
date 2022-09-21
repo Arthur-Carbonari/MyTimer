7af29936ed8fbd2f0837baa0f299377bbb04d8dd
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 11:31:24 -0600

    Implement testIsArgsValid

--- MyTimer/src/ArgsParserTest.java
+++ MyTimer/src/ArgsParserTest.java
@@ -3,10 +3,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ArgsParserTest {
+	
+	static String[][] mockValidArgsArray = {{"1.1"}, {".1"}, {"2,3"}, {"03:12"}, {"11/11"}, {"10"}};
 
 	@Test
 	void testIsArgsValid() {
-		fail("Not yet implemented");
+		
+		boolean isValid = true;
+		
+		for (String[] mockArgs : mockValidArgsArray) {
+			
+			if(!ArgsParser.isArgsValid(mockArgs)) {
+				isValid = false;
+				break;
+			}
+			
+		}
+		
+		
+		assertTrue(isValid);
 	}
 
 	@Test
