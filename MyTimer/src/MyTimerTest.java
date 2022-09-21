3ed7edbc50cf337eb7312f5baf4c6392c4170d73
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 16:30:31 -0600

    Create MyTimerTest unit test

---
+++ MyTimer/src/MyTimerTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyTimerTest {
+
+	@Test
+	void testMyTimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStartTimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMyTimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTimeInMilliseconds() {
+		fail("Not yet implemented");
+	}
+
+}
