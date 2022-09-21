d5c04b607dee403c17eb7fb15af7fb95a08c7edc
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 15:40:27 -0600

    Implement parseArgsToIntArray method

--- MyTimer/src/ArgsParser.java
+++ MyTimer/src/ArgsParser.java
@@ -6,7 +6,6 @@ public class ArgsParser {
 		if(args.length != 1) return false;
 		
 		String arg = args[0];
-		System.out.println(arg);
 		
 		if(!arg.matches("^\\d{1,3}([:.,]\\d{0,3})?$|^[:.,]\\d{1,3}$")) return false;
 		
@@ -15,6 +14,32 @@ public class ArgsParser {
 
 	public static int[] parseArgsToIntArray(String[] args) {
 		
-		return new int[2];
+		String timeString = args[0];
+		
+		int[] intArray = {0 , 0};
+		
+		if(timeString.matches("^[,.:]?\\d{1,3}$")) {
+			
+			timeString = timeString.replaceAll("[,.:]", "");
+			
+			intArray[1] = Integer.parseInt(timeString);
+			
+			return intArray;
+		}
+		
+		if(timeString.matches("^\\d{1,3}[:.,]\\d{0,3}$")) {
+			
+			String[] splitTimeString = timeString.split("[:.,]", 2);
+						
+			if(splitTimeString[1] == "") splitTimeString[1] = "0";
+			
+			intArray[0] = Integer.parseInt(splitTimeString[0]);
+			intArray[1] = Integer.parseInt(splitTimeString[1]);
+			return intArray;
+		}
+		
+		
+		throw new IllegalArgumentException("Argument in the wrong format.");
+		
 	}
 }
