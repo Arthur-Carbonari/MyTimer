afdd1a6bad11f45c65000ffb0d2d4d2e2e3a2dff
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 14:21:34 -0600

    Implement isArgsValid method

--- MyTimer/src/ArgsParser.java
+++ MyTimer/src/ArgsParser.java
@@ -3,7 +3,14 @@ public class ArgsParser {
 
 	public static boolean isArgsValid(String[] args) {
 		
-		return false;
+		if(args.length != 1) return false;
+		
+		String arg = args[0];
+		System.out.println(arg);
+		
+		if(!arg.matches("^\\d{1,3}([:.,]\\d{0,3})?$|^[:.,]\\d{1,3}$")) return false;
+		
+		return true;
 	}
 
 	public static int[] parseArgsToIntArray(String[] args) {
