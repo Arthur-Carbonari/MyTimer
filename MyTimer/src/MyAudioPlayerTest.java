cd4f9fb078af6df7a222c0690866a25e134bc970
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 20:05:28 -0600

    Create Unit Test MyAudioPlayerTest

---
+++ MyTimer/src/MyAudioPlayerTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyAudioPlayerTest {
+
+	@Test
+	void testLoadAudioFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlayAudio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStopAudio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsPlaying() {
+		fail("Not yet implemented");
+	}
+
+}
