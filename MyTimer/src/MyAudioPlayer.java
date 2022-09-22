7adf16a37eaf243e084483306c8e5f788eb574cb
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 20:04:38 -0600

    Create class MyAudioPlayer

---
+++ MyTimer/src/MyAudioPlayer.java
@@ -0,0 +1,29 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+
+public class MyAudioPlayer{
+	
+	private AudioInputStream audioStream;
+	private Clip audioClip;
+	
+	public boolean loadAudioFile(String pathname) {
+		
+		return false;
+	}
+	
+	public boolean playAudio() {
+		
+		return false;
+	}
+	
+	public boolean stopAudio() {
+		
+		return false;
+	}
+	
+	public boolean isPlaying() {
+		return false;
+	}
+	
+
+}
