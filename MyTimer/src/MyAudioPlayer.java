3046ab22b93b88ee91e211347e388b19a652d5c5
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-21 20:41:31 -0600

    Implement loadAudioFile method

--- MyTimer/src/MyAudioPlayer.java
+++ MyTimer/src/MyAudioPlayer.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class MyAudioPlayer{
 	
@@ -8,7 +16,24 @@ public class MyAudioPlayer{
 	
 	public boolean loadAudioFile(String pathname) {
 		
-		return false;
+		File audioFile = new File(pathname);
+		System.out.println(audioFile);
+		
+		try {
+			audioStream = AudioSystem.getAudioInputStream(audioFile);
+			
+			AudioFormat format = audioStream.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			
+			audioClip = (Clip) AudioSystem.getLine(info);
+			
+			return true;
+			
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
 	}
 	
 	public boolean playAudio() {
