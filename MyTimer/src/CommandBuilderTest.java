06d36ef154cbf29b13600dc6e908a46441f8edf9
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-22 20:29:56 -0600

    Create test class for CommandBuilder

---
+++ MyTimer/src/CommandBuilderTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CommandBuilderTest {
+
+	@Test
+	void testGetRunJavaClassCommand() {
+		fail();
+	}
+	
+	@Test
+	void testGetRunJavaClassCommand2() {
+		fail();
+	}
+
+}
