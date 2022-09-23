f96098b245426e720ab19d9613991be2e4f9d558
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-22 20:05:51 -0600

    Create class TimerPopup

---
+++ MyTimer/src/TimerPopup.java
@@ -0,0 +1,19 @@
+import java.awt.Point;
+
+
+
+
+public class TimerPopup {
+
+
+	
+	public static void display(String message) {
+			
+	}
+	
+	private static Point bottomRightPoint() {
+
+		return null;
+	}
+
+}
