caa37dd3dee3780a16a6794581682ab43fc018b0
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-09-22 20:35:53 -0600

    Implement unit tests for getRunJavaClassCommand

--- MyTimer/src/CommandBuilderTest.java
+++ MyTimer/src/CommandBuilderTest.java
@@ -6,12 +6,24 @@ class CommandBuilderTest {
 
 	@Test
 	void testGetRunJavaClassCommand() {
-		fail();
+		
+		String expected = "javaw -cp C:/test; Test 1 2";
+		String[] args = {"1" , "2"};
+		
+		String actual = CommandBuilder.getRunJavaClassCommand("C:/test", "Test", args);
+		
+		assertEquals(expected, actual);
 	}
 	
 	@Test
 	void testGetRunJavaClassCommand2() {
-		fail();
+		String expected = "javaw -cp /C:/Users/arthu/git/MyTimer/MyTimer/bin/; MyTimer 5 6";
+		
+		String[] args = {"5", "6"};
+		
+		String actual = CommandBuilder.getRunJavaClassCommand("/C:/Users/arthu/git/MyTimer/MyTimer/bin/", "MyTimer", args);
+	
+		assertEquals(expected, actual);
 	}
 
 }
